1d1384778c754af6fd45030c7be8e80b0d55c98e
grela <grelam100@gmail.com>
2024-06-19 17:21:23 +0200

    Initial setup

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+import uvicorn
+
+from app.api.main import api_router
+from app.core.config import settings
+
+app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+app.include_router(api_router, prefix='/api')
+
+if __name__ == "__main__":
+    if settings.environment == "development":
+        reload = True
+    else:
+        reload = False
+
+    uvicorn.run(
+        settings.app_run_name,
+        host=settings.host,
+        port=settings.api_port,
+        reload=reload,
+    )
